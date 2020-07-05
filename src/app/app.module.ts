388f891864a38c0fd39624d482ffec329e4880b5
victor <felipe.beleza29@gmail.com>
2020-07-05 15:52:57 -0400

    Adicionado pagina inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaInicialComponent,
   ],
   imports: [
     BrowserModule,
